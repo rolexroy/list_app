87e3854340707e771fe4a054cd34b5a5bb0e6559
rolexroy <rolexroney@gmail.com>
2021-03-19 17:40:07 +0300

    added partials & parsed data

--- app.js
+++ app.js
@@ -4,21 +4,19 @@ const app = express();
 app.use(express.static('public'))
 app.set('views', './views');
 app.set('view engine', 'ejs');
-app.get('/', (req , res ) => {
-   
-    res.render("index",{name : "vitu fishi"})
 
+const items = [{id:1, name:'potatoes'},{id:2, name:'chilli'},{id:3, name:'yams'}]
+
+app.get('/', (req , res ) => {  
+    res.render("index",{name : "vitu fishi"})
 });
+
 app.get('/items',(req,res) =>{
-    res.render('items');
+    res.render('items' , {items: items});
 })
-app.get('/potatoes',(req,res) =>{
-    res.render('potatoes');
+
+app.get('/create', (req,res) => {
+    res.render('add');
 });
-app.get('/chilli', (req, res) => {
-    res.render('chilli');
-})
-app.get('/yams', (req, res) => {
-    res.render('yams');
-})
+
 app.listen(3000)
