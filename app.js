3be1eab7da9061bb962410f08d93446d29118d03
rolexroy <rolexroney@gmail.com>
2021-03-26 20:44:25 +0300

    added form & push new item to list

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const app = express();
 app.use(express.static('public'))
+
+//Config to get access to form values
+app.use(express.urlencoded({extended: false}))
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
@@ -11,12 +14,23 @@ app.get('/', (req , res ) => {
     res.render("index",{name : "vitu fishi"})
 });
 
-app.get('/items',(req,res) =>{
+app.get('/items',(req,res) => {
     res.render('items' , {items: items});
 })
 
+//grab form
 app.get('/create', (req,res) => {
-    res.render('add');
+    res.render('create');
 });
 
-app.listen(3000)
+//submit form
+app.post('/create',(req, res) =>{
+    //grab input data & add to the list
+    let count = items.length + 1;
+    items.push({id: count, name: req.body.newItem});
+    // console.log(items);
+    //redirect to items page
+    res.redirect('items');
+ 
+});
+app.listen(8080);
