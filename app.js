20f12a796df6af056acee6be089ff4895401f10c
rolexroy <rolexroney@gmail.com>
2021-04-01 16:48:21 +0300

    added node js driver for mysql

--- app.js
+++ app.js
@@ -1,32 +1,64 @@
 
 const express = require("express");
+const mysql = require('mysql');
+
 const app = express();
-app.use(express.static('public'))
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'list_app'
+});
+
+// connection.connect();
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+  
+    console.log('connected as id ' + connection.threadId);
+  });
+
+app.use(express.static('public'));
 
 //Config to get access to form values
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 const items = [{id:1, name:'potatoes'},{id:2, name:'chilli'},{id:3, name:'yams'}]
 
 app.get('/', (req , res ) => {  
-    res.render("index",{name : "vitu fishi"})
+    res.render("index",{name : "vitu fishi"});
 });
 
 app.get('/items',(req,res) => {
-    res.render('items' , {items: items});
+    // res.render('items' , {items: items});
+    connection.query(
+        'SELECT * FROM items',
+        (error, results) => {
+            console.log(results);
+        }
+    );
 });
 
 app.get('/items/:id',(req,res) => {
+    // res.status(404).send("Sorry can't find that!")
     // get route parameter (id)
     // console.log(req.params.id);
     let id = Number(req.params.id);
     let item = items.find(item => item.id === id);
     // console.log(req.params.id);
-
+    if(item){
+        res.render ('item' , {item : item});
+    }
+    else{
+        res.render ('error');
+    }
     // console.log(item);
-    res.render ('item' , {item : item});
+    
 })
 
 //grab form
@@ -43,5 +75,10 @@ app.post('/create',(req, res) =>{
     //redirect to items page
     res.redirect('items');
  
+});
+
+//update item
+app.get('/update/:id', (req, res) => {
+    res.render('edit');
 });
 app.listen(8080);
